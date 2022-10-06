e25f85f2f33f3fedbf3f9ea518f095d983669ff0
Emmanuel Alawode <alawodeemmanuel2@gmail.com>
2022-10-06 12:50:02 +0100

    fix: small thing

--- components/landing/blog/Latest.component.js
+++ components/landing/blog/Latest.component.js
@@ -18,7 +18,7 @@ export default function Latest() {
   return (
     <div className='relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8 w-full'>
       <div className='absolute inset-0'>
-        <div className='bg-white h-1/3 sm:h-2/3' />
+        <div className=' h-1/3 sm:h-2/3' />
       </div>
       <div className='relative max-w-7xl mx-auto'>
         <div className='text-center'>
@@ -30,14 +30,13 @@ export default function Latest() {
             with latest announcments, data, events, news and many more.
           </p>
         </div>
-        {/* <ReactMarkdown>{posts[0].attributes?.Content}</ReactMarkdown> */}
         <div className='mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none'>
           {posts.map((post, index) => (
             <div
               key={index}
               className='flex flex-col rounded-2xl shadow-lg overflow-hidden transition-all duration-300 hover:shadow-2xl border'
             >
-              <div className='flex-shrink-0 relative h-60 w-full shadow-sm border'>
+              <div className='flex-shrink-0 relative h-60 w-full shadow-sm border border'>
                 <Image
                   className='h-48 w-full object-cover'
                   src={post.attributes.image.data.attributes.url}
@@ -45,7 +44,7 @@ export default function Latest() {
                   layout='fill'
                 />
               </div>
-              <div className='flex-1 bg-white p-6 flex flex-col justify-between'>
+              <div className='flex-1 bg-white-100 p-6 flex flex-col justify-between'>
                 <div className='flex-1'>
                   <p className='text-sm font-medium text-color-5'>
                     {post.attributes?.Type}
